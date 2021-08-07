5daf553fb5ba808ba11c696191661ea9cd5c4191
Arielson Oliveira <aryelnti@gmail.com>
2021-08-07 18:02:29 -0300

    update repo

--- k8s_terraform/k8s-do/k8s.tf
+++ k8s_terraform/k8s-do/k8s.tf
@@ -1,7 +1,6 @@
 resource "digitalocean_kubernetes_cluster" "hello-world" {
   name   = "hello-world"
   region = "nyc1"
-  # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.21.2-do.2"
 
   node_pool {
